98a29a59bf74d94af12805e687b2df80a196a0eb
hijeong_ <gmlwjdchl82@naver.com>
2022-07-25 12:22:15 +0900

    header

--- src/components/pages/Header.js
+++ src/components/pages/Header.js
@@ -1,3 +1,39 @@
+import styled from "styled-components";
+import { mainStyle } from "../../style/GlobalStyle";
+
+const Wrap = styled.div`
+  height: 80px;
+  width: 100%;
+  background-color: lightgray;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: ${mainStyle.padding};
+`;
+const Logo = styled.h3`
+  font-size: 30px;
+  font-weight: 900;
+`;
+const MenuWrap = styled.ul`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+const Menu = styled.li`
+  font-size: 22px;
+  font-weight: 900;
+  margin-left: 30px;
+`;
+
 export const Header = () => {
-  return <>Header</>;
+  return (
+    <Wrap>
+      <Logo>LOGO</Logo>
+      <MenuWrap>
+        <Menu>menu</Menu>
+        <Menu>menu</Menu>
+        <Menu>menu</Menu>
+      </MenuWrap>
+    </Wrap>
+  );
 };
